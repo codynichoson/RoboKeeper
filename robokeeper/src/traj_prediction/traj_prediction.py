ae6fcbd4e84a2d3f0d7498df0841f3f4d01d6b49
Jonny <jonnyb@u.northwestern.edu>
2021-11-22 11:04:28 -0600

    cleaned up/ slightly reformatted traj prediction

--- robokeeper/src/traj_prediction/traj_prediction.py
+++ robokeeper/src/traj_prediction/traj_prediction.py
@@ -2,33 +2,30 @@
 Uses linear or quadratic fitting to predict the position of the ball at the goal line
 '''
 import numpy as np
-import sympy as sym
-import random
 from sklearn import linear_model
 import matplotlib.pyplot as plt
-from sympy.core.symbol import Symbol
-import rosbag
-import bagpy
 from bagpy import bagreader
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from datetime import datetime
 
-def traj_2D(x, y, x_goal):
+def traj_linear_reg(x_train, y_train, x_goal):
     '''Predicts the y position of the ball when it crosses the goal line by fitting
     a line to the first few xy-positions of the ball
 
     Args:
-        x (list): x coordinate training data
-        y (list): y coordinate training data
+        x_train (list): x coordinate training data
+        y_train (list): y coordinate training data
         x_goal (float): position of the goal on the x-axis
 
     Returns:
         y_predict (float): predicted y postion of the ball when it crosses the goal
     '''
+    # Format the input data for the linear regression model function
+    x_train = np.array([x_train]).T
+    y_train = np.array(y_train)
+
     # Compute linear regression model
     reg = linear_model.LinearRegression()
-    reg.fit(x, y)
+    reg.fit(x_train, y_train)
     intercept = reg.intercept_
     slope = reg.coef_
 
@@ -53,17 +50,15 @@ def main():
     Used for testing
     '''
     data = get_rosbag_data().to_numpy()
-    print(datetime.now())
-    x = data[:, 1]
-    y = data[:, 2]
+    x_data = data[:, 1]
+    y_data = data[:, 2]
 
     n = 10
-    x_train = np.array([x[0:n]]).T
-    y_train = y[0:n]
+    x_train = x_data[0:n]
+    y_train = y_data[0:n]
 
     x_goal = -1.27  # As of Nov 21, 8pm
-    y_predict = traj_2D(x_train, y_train, x_goal)
-    print(datetime.now())
+    y_predict = traj_linear_reg(x_train, y_train, x_goal)
 
     print(y_predict)
 
