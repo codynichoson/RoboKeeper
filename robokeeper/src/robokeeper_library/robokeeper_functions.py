c2477110f8f97a964d11fcfa699cbe30d5bb71c6
Jonny <jonnyb@u.northwestern.edu>
2021-11-29 15:24:14 -0600

    arm movement is now limited to the goal limits

--- robokeeper/src/robokeeper_library/robokeeper_functions.py
+++ robokeeper/src/robokeeper_library/robokeeper_functions.py
@@ -146,6 +146,7 @@ def main():
     plt.scatter(x_train2, y_train2, color='red')
     plt.scatter(x_train3, y_train3, color='green')
     plt.scatter(x_train4, y_train4, color='yellow')
+    plt.scatter(x_train5, y_train5, color='orange')
     plt.xlabel('x position (m)')
     plt.ylabel('y position (m)')
     plt.show()
