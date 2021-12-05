b1626c98837238b2809158f00a44fa75a4763366
Joshua Cho <joshuasrcho@gmail.com>
2021-12-05 13:04:00 -0600

    adding unittest capabilities

---
+++ robokeeper/test/robokeeper_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+PKG = 'robokeeper'
+
+import sys
+import unittest
+import rospy
+
+from std_srvs.srv import Empty
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from robokeeper_library.robokeeper_functions import joint_lookup_table, traj_linear_reg
+
+class RobokeeperTest(unittest.TestCase):
+
+    def __init__(self, *args):
+        super(RobokeeperTest,self).__init__(*args)
+        rospy.init_node("robokeeper_test")
+        rospy.sleep(3)
+
+    def test_reset(self):
+        # wait for reset service to become available
+        rospy.wait_for_service("reset")
+        # call reset service
+        rst = rospy.ServiceProxy("reset",Empty)
+        empty_resp = rst()
+
+        JointState_msg = JointTrajectory()
+        JointState_msg.joint_names = ['joint1','joint2','joint3','joint4','joint5','joint6']
+        value = [-3.450028078456225, 1.1812012540734964, 2.8532913410087053, 3.1416885303149615, -0.6757391589862014, -1.2594366618108317]
+        JointTrajectoryPoint_msg = JointTrajectoryPoint()
+        JointTrajectoryPoint_msg.positions = value
+        JointTrajectoryPoint_msg.time_from_start.nsecs = 10000000
+        JointState_msg.points = [JointTrajectoryPoint_msg]
+
+        # wait for message
+        data = rospy.wait_for_message("arm_controller/command",JointTrajectory)
+        self.assertEquals(data,JointState_msg)
+
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, 'robokeeper_test', RobokeeperTest)
