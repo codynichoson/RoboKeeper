b06234f7830a59c841e590738beba1b09f18c486
Joshua Cho <joshuasrcho@gmail.com>
2021-12-05 16:38:12 -0600

    keep tests working

--- robokeeper/test/robokeeper_test.py
+++ robokeeper/test/robokeeper_test.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # PKG = 'robokeeper'
 
-import sys
 import unittest
 import rospy
 
-from std_srvs.srv import Empty
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from robokeeper.srv import Keeper
 from robokeeper_library.robokeeper_functions import joint_lookup_table, traj_linear_reg
 
 class RobokeeperTest(unittest.TestCase):
@@ -16,28 +14,21 @@ class RobokeeperTest(unittest.TestCase):
         rospy.init_node("robokeeper_test")
         rospy.sleep(3)
 
-    def test_reset(self):
-        # wait for reset service to become available
-        rospy.wait_for_service("reset")
-        # call reset service
-        rst = rospy.ServiceProxy("reset",Empty)
-        empty_resp = rst()
+    def test_home(self):
+        # wait for keep service to become available
+        rospy.wait_for_service("keep")
+        # call keep service
+        kpr = rospy.ServiceProxy("keep",Keeper)
+        resp = kpr(0.0)
+        self.assertEquals(1, resp.code.val)
 
-        JointState_msg = JointTrajectory()
-        JointState_msg.joint_names = ['joint1','joint2','joint3','joint4','joint5','joint6']
-        value = [-3.450028078456225, 1.1812012540734964, 2.8532913410087053, 3.1416885303149615, -0.6757391589862014, -1.2594366618108317]
-        JointTrajectoryPoint_msg = JointTrajectoryPoint()
-        JointTrajectoryPoint_msg.positions = value
-        JointTrajectoryPoint_msg.time_from_start.nsecs = 10000000
-        JointState_msg.points = [JointTrajectoryPoint_msg]
-
-        # wait for message
-        data = rospy.wait_for_message("arm_controller/command",JointTrajectory)
-        self.assertEquals(data,JointState_msg)
-        # self.assertEquals(1,2)
-
-    def test_whatever(self):
-        # you can start writing your code here! :)
+    def test_keep(self):
+        # wait for keep service to become available
+        rospy.wait_for_service("keep")
+        # call keep service
+        kpr = rospy.ServiceProxy("keep",Keeper)
+        resp = kpr(0.2)
+        self.assertEquals(1, resp.code.val)
 
 
 if __name__ == '__main__':
